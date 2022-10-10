cb005028e5e107a7cc9147815c6a6789207720be
thegenghiskahn <stmbkahn2019@gmail.com>
2022-10-10 12:09:41 -0400

    set SSL redirect in settings.py

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -26,6 +26,9 @@ DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
 ALLOWED_HOSTS = ['*']
 
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Application definition
 
