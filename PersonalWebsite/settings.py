bb0c738911ed0179c2d69b319a3816cf1d8920c5
thegenghiskahn <stmbkahn2019@gmail.com>
2022-10-12 14:23:55 -0500

    readded https thing

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -27,13 +27,14 @@ DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 ALLOWED_HOSTS = ['*']
 
 
-'''
-#SECURE_SSL_REDIRECT = True
+
+SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 '''
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 SECURE_SSL_REDIRECT = False
+'''
 
 # Application definition
 
